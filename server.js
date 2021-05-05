04b77a81704edbdde9deed720e965b34f30f123a
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-05-05 15:31:38 -0400

    edited and added files

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const homeRoute = require('./routes/home')
 const gamesRoute = require('./routes/games')
+const profileRoute = require('./routes/profile')
 
 require('dotenv').config({path: './config/.env'})
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/', homeRoute)
-
 app.use('/search', gamesRoute)
+app.use('/profile', profileRoute)
 
 app.listen(process.env.PORT, () => {
   console.log('server running')
