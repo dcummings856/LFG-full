8798219773c6b120a1d196e62d6829c4e570584f
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-15 13:13:47 -0400

    fetch data added to ejs file

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,9 +6,8 @@ module.exports = {
       const response = await fetch("https://api.rawg.io/api/games?key=2616f5758f784d868c63a14aa6ad4f69", {
         "method": "GET",
       })
-      const results = await response.json()
-      console.log(results)
-      const games = await results
+      const games = await response.json()
+      console.log(games)
       res.render('profile.ejs', { games: games })
     }catch(err){
       console.log(err)
