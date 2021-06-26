dd01319afb040f6ce7dc0b16dc3609d05b68c082
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-26 08:34:41 -0400

    changed gameinfo to info

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ const homeController = require('../controllers/home')
 router.get('/', homeController.getHome)
 router.get('/nextPage', homeController.getNextPage)
 router.get('/prevPage', homeController.getPrevPage)
-router.get('/gameinfo/:gameID', homeController.getGameInfo)
+router.get('/info/:gameID', homeController.getGameInfo)
 
 
 module.exports = router
