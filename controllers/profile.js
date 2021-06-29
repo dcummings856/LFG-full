96fd884bbdd35e3d813472c92e162b700da62d41
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-06-29 10:58:54 -0400

    profile changes

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,12 +4,13 @@ const GamesList = require('../models/GamesList')
 module.exports = {
   getProfile: async (req, res) => {
     try{
-      const response = await fetch("https://api.rawg.io/api/games?key=2616f5758f784d868c63a14aa6ad4f69", {
-        "method": "GET",
-      })
-      const games = await response.json()
-      console.log(games)
-      res.render('profile.ejs', { games: games })
+      const list = await GamesList.find({ user: req.user.id })
+      // const response = await fetch("https://api.rawg.io/api/games?key=2616f5758f784d868c63a14aa6ad4f69", {
+      //   "method": "GET",
+      // })
+      // const games = await response.json()
+      // console.log(games)
+      res.render('profile.ejs', { list: list, user: req.user })
     }catch(err){
       console.log(err)
     }
